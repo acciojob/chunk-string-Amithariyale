ef4e7e6190658079ea367eaf7d642f2b9b64236d
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2023-12-14 20:26:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function stringChop(str, size) {
   // your code here
-	if(str===null)return [];
+	if(!str)return [];
 	let ans=[];
 	for(let i=0;i<str.length;i+=size){
 		let s;
@@ -11,8 +11,7 @@ function stringChop(str, size) {
 	return ans;
 }
 
-i got only 39 marks and then it is failing 
 // Do not change the code below
-// const str = prompt("Enter String.");
-// const size = Number(prompt("Enter Chunk Size."));
+const str = prompt("Enter String.");
+const size = Number(prompt("Enter Chunk Size."));
 alert(stringChop(str, size));
